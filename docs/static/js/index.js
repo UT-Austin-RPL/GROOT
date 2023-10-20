77daae745755e58b93974804622f1da61ab2936b
Yifeng Zhu <yifeng.zhu@utexas.edu>
2023-10-19 20:45:30 -0500

    update website

--- docs/static/js/index.js
+++ docs/static/js/index.js
@@ -65,13 +65,13 @@ $(document).ready(function() {
         player.currentTime = player.duration / 100 * this.value;
       })
     }, false);*/
-    preloadInterpolationImages();
+    // preloadInterpolationImages();
 
-    $('#interpolation-slider').on('input', function(event) {
-      setInterpolationImage(this.value);
-    });
-    setInterpolationImage(0);
-    $('#interpolation-slider').prop('max', NUM_INTERP_FRAMES - 1);
+    // $('#interpolation-slider').on('input', function(event) {
+    //   setInterpolationImage(this.value);
+    // });
+    // setInterpolationImage(0);
+    // $('#interpolation-slider').prop('max', NUM_INTERP_FRAMES - 1);
 
     // bulmaSlider.attach();
 
@@ -254,7 +254,14 @@ $(document).ready(function() {
       yanchor: 'top',
       // title: ' Success rates (%) of GROOT in realrobot tasks.',        
       showarrow: false,
-
+      xaxis: {
+        tickfont: {
+          size: 24, 
+          family: 'Arial', 
+          color: 'black',
+          weight: 'bold' 
+        }
+      }
     };
 
     Plotly.newPlot('real-robot-results-div', data, layout);
