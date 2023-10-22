61f37942bc3b6c714a6c2c772123d8180f2b7dc1
Yifeng Zhu <yifeng.zhu@utexas.edu>
2023-10-22 10:16:46 -0500

    Update files

--- vos_3d_algo/vos_3d_transformer/__init__.py
+++ vos_3d_algo/vos_3d_transformer/__init__.py
@@ -1,9 +1,2 @@
-# from .vos_3d_wrist_transformer_policy_v2 import *
-# from .vos_3d_real_robot_object_transformer_policy import *
-# from .vos_3d_object_transformer_policy import *
-# from .vos_3d_real_robot_masked_no_wrist_transformer_policy import *
-# from .vos_3d_robot_masked_transformer_policy import *
-# from .vos_3d_robot_masked_no_wrist_transformer_policy import *
-
 from .vos_3d_transformer_algo import VOS3DSingleTask
 from .groot_real_robot_no_wrist_transformer_policy import GROOTRealRobotNoWristTransformerPolicy
